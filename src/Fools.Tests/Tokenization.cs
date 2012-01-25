db817c9ea1f8fed90e46bf2c612fbba2ad5cb15e
Arlo Belshee <arlo.belshee@live.com>
2012-01-25 09:33:49 -0800

    Refactored tests for readability.

--- src/Fools.Tests/Tokenization.cs
+++ src/Fools.Tests/Tokenization.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
 using Fools.Tokenization;
 using NUnit.Framework;
 
@@ -11,15 +13,26 @@ namespace Fools.Tests
 		public void EmptyFileShouldTokenizeToSingleEmptyStatementWithNoIndentation()
 		{
 			var tokens = new FoolsTokenStream("");
-			tokens.Tokens.Should().Equal(new Token[] {new IndentationToken(0), new EndOfStatementToken()});
+			tokens.Tokens.Should().Equal(EmptyLine);
 		}
 
 		[Test]
 		public void OneCharacterFileShouldBeSingleStatementWithThatToken()
 		{
 			var tokens = new FoolsTokenStream("a");
-			tokens.Tokens.Should().Equal(
-				new Token[] {new IndentationToken(0), new IdentifierToken("a"), new EndOfStatementToken()});
+			tokens.Tokens.Should().Equal(LineContaining(Identifier("a")));
+		}
+
+		private static IEnumerable<Token> EmptyLine { get { return LineContaining(); } }
+
+		private static IEnumerable<Token> LineContaining(params Token[] tokens)
+		{
+			return new Token[] {new IndentationToken(0)}.Concat(tokens).Concat(new Token[] {new EndOfStatementToken()});
+		}
+
+		private static IdentifierToken Identifier(string value)
+		{
+			return new IdentifierToken(value);
 		}
 	}
 }
