9aee56d68e742f11d7dc7068bb44261d79640a8d
Arlo Belshee <arlo.belshee@live.com>
2012-02-07 14:01:57 -0800

    Made one test cover the case with multiple statements in a block's body.

--- src/Fools.Tests/BlockAndStatementParsing.cs
+++ src/Fools.Tests/BlockAndStatementParsing.cs
@@ -28,12 +28,14 @@ namespace Fools.Tests
 		{
 			Lines(
 				Line(0, Identifier("some"), Identifier("block.header"), Identifier(":")),
-				Line(1, Identifier("pass"))
+				Line(1, Identifier("do.something")),
+				Line(1, Identifier("do.something.else"))
 				)
 				.ShouldBeRecognizedAs(
 					new Block(
 						With.Tokens(Identifier("some"), Identifier("block.header")),
-						Statement(Identifier("pass"))));
+						Statement(Identifier("do.something")),
+						Statement(Identifier("do.something.else"))));
 		}
 
 		[Test]
