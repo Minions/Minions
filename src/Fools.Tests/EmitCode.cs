85d6e7547c2b41600539991f0d2204b09cd964f7
Arlo Belshee <arlo.belshee@live.com>
2012-02-07 21:53:09 -0800

    Moved the tests related to recognition into their own directory, because they are all related.

--- src/Fools.Tests/EmitCode.cs
+++ src/Fools.Tests/EmitCode.cs
@@ -10,17 +10,21 @@ namespace Fools.Tests
 	[TestFixture, Ignore, UseReporter(typeof(QuietReporter))]
 	public class EmitCode
 	{
-		[Test] public void assignment() { ApproveResultOfExecution(new AssignmentStatement{ variable = "a", value = new NumberLiteral(3)}); }
+		[Test]
+		public void assignment()
+		{
+			ApproveResultOfExecution(new AssignmentStatement {variable = "a", value = new NumberLiteral(3)});
+		}
 
 		private static void ApproveResultOfExecution(INode node)
 		{
-			var frame = _evaluate_and_return_frame(node);
+			Dictionary<string, object> frame = _evaluate_and_return_frame(node);
 			Approvals.Approve(frame, kv => string.Format("{0} = {1} [{2}]", kv.Key, kv.Value, kv.Value.GetType()));
 		}
 
 		private static Dictionary<string, object> _evaluate_and_return_frame(INode node)
 		{
-			var method = _make_an_assignment(node);
+			DynamicMethod method = _make_an_assignment(node);
 			var frame = new Dictionary<string, object>();
 			method.Invoke(null, new[] {frame});
 			return frame;
