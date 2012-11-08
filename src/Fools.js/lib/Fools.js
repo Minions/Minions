e28bc4a5d435b5a6d1e57062c14512f698d79c42
Arlo Belshee <arlo.belshee@live.com>
2012-11-08 06:31:38 -0800

    Restored the BSD license. Grunt inited a Fools.js. Just like Grunt built it. Have not yet coffeed it.

---
+++ src/Fools.js/lib/Fools.js
@@ -0,0 +1,15 @@
+/*
+ * Fools
+ * https://github.com/arlobelshee/Minions
+ *
+ * Copyright (c) 2012 Arlo Belshee
+ * Licensed under the BSD license.
+ */
+
+(function(exports) {
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
