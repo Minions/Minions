d013c18d78b147b1622bf0d2df83350ee74c8a70
Arlo Belshee <arlo.belshee@live.com>
2013-03-30 12:42:45 -0700

    Added a trivial test.

---
+++ src/Fools.cs.Tests/FindBlocksAndLines.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Fools.cs.Tests
+{
+	[TestFixture]
+	public class FindBlocksAndLines
+	{
+		[Test]
+		public void ShouldFindNothingInAnEmptyFile()
+		{
+			FoolsParser.FindBlocks(string.Empty).Should().BeEmpty();
+		}
+
+	}
+
+	public class FoolsParser
+	{
+		public static IEnumerable<string> FindBlocks(string sourceCode)
+		{
+			return Enumerable.Empty<string>();
+		}
+	}
+}
