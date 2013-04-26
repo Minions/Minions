0dbd293bfc65f6a81814a0d178ca928ae6b973d8
Arlo Belshee <arlo.belshee@live.com>
2013-04-26 07:39:32 -0700

    Extracted the interpreter classes to their own files and moved them to the right project.

---
+++ src/Fools.cs/Interpret/Interpreter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fools.cs.Interpret
+{
+    public class Interpreter : IDisposable
+    {
+        public TestControlCenter tests = new TestControlCenter();
+
+        public void Dispose()
+        {
+            tests.Dispose();
+        }
+
+        public void take_commands(string fools_command_file_contents)
+        {
+        }
+    }
+}
---
+++ src/Fools.cs/Interpret/TestControlCenter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Fools.cs.Interpret
+{
+    public class TestControlCenter : IDisposable
+    {
+        private readonly List<TestResult> _results = new List<TestResult>();
+
+        public ReadOnlyCollection<TestResult> results { get { return _results.AsReadOnly(); } }
+
+        public void execute()
+        {
+            _results.Add(TestResult.passed("the test framework", "a simple empty test should pass"));
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
