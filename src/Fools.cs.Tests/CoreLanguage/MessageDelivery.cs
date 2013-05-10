a7cb1d39cfdf087090945f361c17f450662edafd
Arlo Belshee <arlo.belshee@live.com>
2013-05-10 09:02:33 -0700

    Starting to extract mail functionality into a mail room.

--- src/Fools.cs.Tests/CoreLanguage/MessageDelivery.cs
+++ src/Fools.cs.Tests/CoreLanguage/MessageDelivery.cs
@@ -16,7 +16,7 @@ namespace Fools.cs.Tests.CoreLanguage
 		[Test]
 		public void announced_messages_should_be_delivered_to_all_who_have_expressed_interest_in_that_message_type()
 		{
-			var mission_control = new MissionControl();
+			var mission_control = new MailRoom();
 			var log = new MessageLog<string>(m => ((SillyMessage) m).value);
 			mission_control.subscribe(log, "SillyMessage");
 			mission_control.announce(new SillyMessage("1"));
@@ -28,7 +28,7 @@ namespace Fools.cs.Tests.CoreLanguage
 		[Test]
 		public void subscribers_should_only_get_messages_they_asked_for()
 		{
-			var mission_control = new MissionControl();
+			var mission_control = new MailRoom();
 			var log = new MessageLog<string>(m => ((SillyMessage) m).value);
 			mission_control.subscribe(log, "SillyMessage");
 			mission_control.announce(new SillyMessage("silly"));
@@ -38,13 +38,13 @@ namespace Fools.cs.Tests.CoreLanguage
 		}
 
 		[Test]
-		public void mail_sent_to_a_building_should_also_be_announced()
+		public void mail_sent_to_a_mail_room_should_also_be_announced_to_all_more_central_mail_rooms()
 		{
-			var mission_control = new MissionControl();
+			var home_office = new MailRoom();
 			var log = new MessageLog<string>(m => ((SillyMessage) m).value);
-			var mail_target = mission_control.create_building();
-			mission_control.subscribe(log, "SillyMessage");
-			mail_target.send(new SillyMessage("hi"));
+			var mail_target = home_office.create_satellite_office();
+			home_office.subscribe(log, "SillyMessage");
+			mail_target.announce(new SillyMessage("hi"));
 			log.received.Should()
 				.ContainInOrder(new object[] {"hi"});
 		}
---
+++ src/Fools.cs/Api/MailRoom.cs
@@ -0,0 +1,54 @@
+﻿// MailRoom.cs
+// 
+// Copyright 2012 The Minions Project (http:/github.com/Minions).
+// All rights reserved. Usage as permitted by the LICENSE.txt file for this project.
+
+using System.Collections.Generic;
+using Fools.cs.Interpret;
+
+namespace Fools.cs.Api
+{
+	public class MailRoom
+	{
+		private readonly MailRoom _home_office;
+
+		private readonly Dictionary<string, List<MessageRecipient>> _listeners =
+			new Dictionary<string, List<MessageRecipient>>();
+
+		private MailRoom(MailRoom home_office)
+		{
+			_home_office = home_office;
+		}
+
+		public MailRoom()
+		{
+			_home_office = null;
+		}
+
+		public void announce(MailMessage what_happened)
+		{
+			if (_home_office != null) _home_office.announce(what_happened);
+			var mesage_type = what_happened.GetType()
+				.Name;
+			if (!_listeners.ContainsKey(mesage_type)) return;
+			_listeners[mesage_type].ForEach(recipient => recipient.accept(what_happened));
+		}
+
+		public void subscribe(MessageRecipient listener, string message_type)
+		{
+			List<MessageRecipient> subscribers;
+			if (_listeners.ContainsKey(message_type)) subscribers = _listeners[message_type];
+			else
+			{
+				subscribers = new List<MessageRecipient>();
+				_listeners[message_type] = subscribers;
+			}
+			subscribers.Add(listener);
+		}
+
+		public MailRoom create_satellite_office()
+		{
+			return new MailRoom(this);
+		}
+	}
+}
