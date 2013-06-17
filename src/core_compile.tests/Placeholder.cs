9d0e06a2b11bf718cd9d9868c74da1ac73946d4e
Arlo Belshee <arlo.belshee@live.com>
2013-06-17 08:15:46 -0700

    Created a bunch of empty projects. the core compiler and its tests. This will not be a full Fools compiler. It will be enough to compile the rest of the compiler (the parts written in Fools) and merge those passes with the ones defined here, creating the real compiler.

---
+++ src/core_compile.tests/Placeholder.cs
@@ -0,0 +1,16 @@
+﻿// Placeholder.cs
+// 
+// Copyright 2012 The Minions Project (http:/github.com/Minions).
+// All rights reserved. Usage as permitted by the LICENSE.txt file for this project.
+
+using NUnit.Framework;
+
+namespace core_compile.tests
+{
+	[TestFixture]
+	public class Placeholder
+	{
+		[Test]
+		public void should_do_nothing() {}
+	}
+}
---
+++ src/core_compile/Program.cs
@@ -0,0 +1,16 @@
+﻿// Program.cs
+// 
+// Copyright 2012 The Minions Project (http:/github.com/Minions).
+// All rights reserved. Usage as permitted by the LICENSE.txt file for this project.
+
+using Fools.cs.Utilities;
+
+namespace core_compile
+{
+	internal class Program
+	{
+		// ReSharper disable InconsistentNaming
+		private static void Main([NotNull] string[] args) // ReSharper restore InconsistentNaming
+		{}
+	}
+}
